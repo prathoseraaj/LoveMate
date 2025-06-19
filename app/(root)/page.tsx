133c96b9df57496f794b77f69c4f2b340eb465e7
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-19 16:51:02 +0530

    feat: ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const page = () => {
+
+
+
   return (
-    <div>page</div>
+    <div className='  h-[100vh] w-full flex justify-center items-center'>
+      <div className=' flex flex-col w-[400px] p-5 h-[60vh] bg-pink-100 shadow-lg'> 
+        <h1 className='text-[25px] font-bold ' >LoveMate ❤️</h1>
+
+        <div className='text-[20px]'>
+          {}
+        </div>
+      </div>
+    </div>
   )
 }
 
