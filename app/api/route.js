71c5d2190978be1e1356fa514a820e4e4620202d
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-19 17:47:22 +0530

    fix: typo

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.gemini_api_key);
 
 export async function POST(request) {
   try {
